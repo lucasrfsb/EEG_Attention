fcd156c04090754106ecafee5f471a6ae383ac83
Lucas <lucas.oct8@gmail.com>
2021-06-26 19:42:11 -0300

    Energy with iteration through a dataset

--- EEG_ENERGY.py
+++ EEG_ENERGY.py
@@ -9,93 +9,91 @@ import scipy
 from pylab import *
 
 df_multi_rest=pd.read_csv('dataset_arith_rest_csv/Subject00_2.csv', sep=',',header=None)
+# df_multi_rest=pd.read_csv('dataset_arith_rest_csv/test_csv.csv', sep=',',header=None)
 # df2_multi_arith=pd.read_csv('f3_cz_arith.csv', sep=',',header=None)
 
-df_rest_fp1=df_multi_rest.iloc[ : , 0:1 ]
+df_multi_rest=df_multi_rest.iloc[ : , 0:6 ]
 
-print (df_rest_fp1)
+def energy_one_sample (sample, sample_max):
 
-# df_arith_fp1=df2_multi_arith.iloc[ : , 1 ]
-data_size = 30000
-data_step = 15000
-data_slices = round(data_size/data_step)
-
-slices_rest_fp1=np.zeros((data_step,data_slices))
-
-#Slice dataset to get more samples
-cont = 0
-line = 1
-while cont < data_slices:
-	slices_rest_fp1[ : , cont]= df_rest_fp1[ line : (line+data_step) ]
-	line = line + data_step
-	cont = cont + 1
-print (slices_rest_fp1)
-print (slices_rest_fp1.shape)
-##
-
-Y_fp1_rest=np.zeros(( round(data_step/2 + 1) ,data_slices))
-cont = 0
-while cont < data_slices:
-	Y_fp1_rest [ : , cont]= rfftn( slices_rest_fp1[ : , cont] )
-	ps_fp1_rest = np.abs(Y_fp1_rest)**2
-	cont = cont + 1
-print (Y_fp1_rest)
-print ("Y_fp1_rest.shape = ", Y_fp1_rest.shape)
-print (ps_fp1_rest)
-print (ps_fp1_rest.shape)
-##
-
-Energy_rest_fp1 = np.zeros((5,data_slices))
-N_=len(ps_fp1_rest)
-
-i=0
-#PSD FP1
-freq_before_alfa = round(N_ * 0.5 / 500)
-freq_alfa_start = round(N_ * 7 / 500)
-freq_alfa_stop = round(N_ * 12 / 500)
-freq_low_beta_stop = round(N_ * 20/ 500)
-freq_high_beta_stop = round(N_ * 30 / 500)
-freq_gamma = round(N_ * 60 / 500)
-
-before_alfa_range = np.arange(freq_before_alfa, freq_alfa_start, 1)
-alfa_range = np.arange(freq_alfa_start, freq_alfa_stop, 1)
-low_beta_range = np.arange(freq_alfa_stop, freq_low_beta_stop, 1)
-high_beta_range = np.arange(freq_low_beta_stop, freq_high_beta_stop, 1)
-gamma_range = np.arange(freq_high_beta_stop, freq_gamma, 1)
-
-cont = 0
-while cont < data_slices:
-	Energy_rest_fp1[ 0 , cont] = ps_fp1_rest[before_alfa_range, cont].sum()
-	Energy_rest_fp1[ 1 , cont] = ps_fp1_rest[alfa_range, cont].sum() 
-	Energy_rest_fp1[ 2 , cont] = ps_fp1_rest[low_beta_range, cont].sum() 
-	Energy_rest_fp1[ 3 , cont] = ps_fp1_rest[high_beta_range, cont].sum() 
-	Energy_rest_fp1[ 4 , cont] = ps_fp1_rest[gamma_range, cont].sum() 
-	cont = cont + 1
-
-print ("ENERGY: ")
-print(Energy_rest_fp1)
-print(Energy_rest_fp1.shape)
-Energy_rest_fp1 = Energy_rest_fp1.transpose()
-np.savetxt("arith_energy_fp1_2_slices.csv", Energy_rest_fp1, delimiter=" ")
-
-# print ("ENERGY: ")
-# print(Energy_rest_fp1[ 0 , 0]) 
-# print(Energy_rest_fp1[ 1 , 0]) 
-# print(Energy_rest_fp1[ 2 , 0] )
-# print(Energy_rest_fp1[ 3 , 0]) 
-# print(Energy_rest_fp1[ 4 , 0] )
+	sample_size = 15000 * sample_max
+	sample_step = 15000
+	fft_size = round(sample_step/2 + 1)
 
 
+	# data_slices = round(data_size/data_step)
 
-# print("========PSD FP1 threshold========")
-# print("arith_before_alfa = ",arith_before_alfa)
-# print("rest_before_beta= ",rest_before_alfa)
-# print("arith_alfa = ",arith_alfa)
-# print("rest_alfa = ",rest_alfa)
-# print("arith_low_beta = ",arith_low_beta)
-# print("rest_low_beta = ",rest_low_beta)
-# print("arith_high_beta = ",arith_high_beta)
-# print("rest_high_beta = ",rest_high_beta)
-# print("arith_after_beta = ",arith_after_beta)
-# print("rest_after_beta = ",rest_after_beta)
-# print("=============================")
+	line_cont = 0
+	peace_cont = 0 #column
+	peaces = 1 
+
+	# print ("df multi rest \n",df_multi_rest)
+	# print ("\ndf multi rest shape ", df_multi_rest.shape)
+
+	np_rest = np.array(df_multi_rest)
+	# print("np shape shape",df_multi_rest.shape)
+
+	##FFT
+
+	temp = zeros (( sample_step,1 ))
+	Y_rest=  np.zeros(( fft_size , 1 ))
+	freq_data = np.zeros (( fft_size , 6 ))
+
+	lines = (sample - 1) * sample_step + 1
+	cont = 0
+
+	while cont < 6:
+		temp  = np_rest[ lines : (lines + sample_step ) , cont ]
+		# print (np_rest[ lines : (lines + data_step - 1), cont  ])
+		Y_rest = rfftn(temp)
+		ps_rest = np.abs(Y_rest)**2
+		freq_data [ 0:fft_size , cont ] = ps_rest
+		cont = cont + 1
+	##
+
+	# print(temp.shape)
+	# print(Y_rest.shape)
+	# print ("freq data",freq_data)
+	# print (" freq_data shape", freq_data.shape)
+	# print(freq_data)
+
+	#ENERGY
+
+	Energy_rest = np.zeros((5,6))
+	N_=len(ps_rest)
+
+	i=0
+
+	freq_before_alfa = round(N_ * 0.5 / 500)
+	freq_alfa_start = round(N_ * 7 / 500)
+	freq_alfa_stop = round(N_ * 12 / 500)
+	freq_low_beta_stop = round(N_ * 20/ 500)
+	freq_high_beta_stop = round(N_ * 30 / 500)
+	freq_gamma = round(N_ * 60 / 500)
+
+	before_alfa_range = np.arange(freq_before_alfa, freq_alfa_start, 1)
+	alfa_range = np.arange(freq_alfa_start, freq_alfa_stop, 1)
+	low_beta_range = np.arange(freq_alfa_stop, freq_low_beta_stop, 1)
+	high_beta_range = np.arange(freq_low_beta_stop, freq_high_beta_stop, 1)
+	gamma_range = np.arange(freq_high_beta_stop, freq_gamma, 1)
+
+	cont = 0
+	while cont < 6:
+		Energy_rest[ 0 , cont] = freq_data[before_alfa_range, cont].sum()
+		Energy_rest[ 1 , cont] = freq_data[alfa_range, cont].sum() 
+		Energy_rest[ 2 , cont] = freq_data[low_beta_range, cont].sum() 
+		Energy_rest[ 3 , cont] = freq_data[high_beta_range, cont].sum() 
+		Energy_rest[ 4 , cont] = freq_data[gamma_range, cont].sum() 
+		cont = cont + 1
+
+	print ("ENERGY: ")
+	print(Energy_rest)
+	print(Energy_rest.shape)
+
+arith_sample = 1
+arith_sample_max = 2
+
+while arith_sample <= arith_sample_max:
+	energy_one_sample (arith_sample, arith_sample_max)
+	arith_sample = arith_sample + 1
+
